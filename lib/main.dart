37e6b74cc62a1274834a1c28752faa7861de33f1
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-12 23:56:24 +0530

    Bloc completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter_infinite_list/app.dart';
+import 'package:flutter_infinite_list/my_app.dart';
 import 'package:flutter_infinite_list/simple_bloc_observer.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
--- lib/posts/view/posts_list.dart
+++ lib/posts/view/posts_list.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_infinite_list/posts/bloc/post_bloc.dart';
+import 'package:flutter_infinite_list/posts/bloc/post_event.dart';
 import 'package:flutter_infinite_list/posts/bloc/post_state.dart';
-
+import 'package:flutter_infinite_list/posts/widgets/bottom_loader.dart';
+import 'package:flutter_infinite_list/posts/widgets/post_widget.dart';
 
 class PostsList extends StatefulWidget {
   @override
@@ -11,44 +13,63 @@ class PostsList extends StatefulWidget {
 
 class _PostsListState extends State<PostsList> {
   final _scrollController = ScrollController();
-  final _scrollThreshold = 200.0,
-
-  PostBloc _postbloc;
+  final _scrollThreshold = 200.0;
+  PostBloc _postBloc;
 
   @override
   void initState() {
-    _scrollController.addListener(_onScroll);
-    _postbloc = BlocProvider.of<PostBloc>(context);
     super.initState();
+    _scrollController.addListener(_onScroll);
+    _postBloc = BlocProvider.of<PostBloc>(context);
   }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<PostBloc, PostState>(
       builder: (context, state) {
-        if(state is PostInitial) {
+        if (state is PostInitial) {
           return Center(
             child: CircularProgressIndicator(),
           );
         }
-        if(state is PostFailure) {
-             return Center(
+        if (state is PostFailure) {
+          return Center(
             child: Text('failed to fetch posts'),
           );
         }
-        if(state is PostSuccess) {
-          if(state.posts.isEmpty) {
-               return Center(
+        if (state is PostSuccess) {
+          if (state.posts.isEmpty) {
+            return Center(
               child: Text('no posts'),
             );
           }
+          return ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return index >= state.posts.length
+                  ? BottomLoader()
+                  : PostWidget(post: state.posts[index]);
+            },
+            itemCount: state.hasReachedMax
+                ? state.posts.length
+                : state.posts.length + 1,
+            controller: _scrollController,
+          );
         }
       },
-    )
-    ();
+    );
   }
-}
 
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
-void _onScroll() {
-
+  void _onScroll() {
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.position.pixels;
+    if (maxScroll - currentScroll <= _scrollThreshold) {
+      _postBloc.add(PostFetched());
+    }
+  }
 }
