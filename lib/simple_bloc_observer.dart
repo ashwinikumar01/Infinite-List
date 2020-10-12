835af7c19c8f8f724981a5d479e5cf7e8ce2f6b7
Ashwini Kumar Priyanshu <priyanshuashwinikumar@gmail.com>
2020-10-12 21:31:16 +0530

    added simple_bloc_observer

---
+++ lib/simple_bloc_observer.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+  void onTransition(Bloc bloc, Transition transition) {
+    print(transition);
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onError(Cubit cubit, Object error, StackTrace stackTrace) {
+    print(error);
+    super.onError(cubit, error, stackTrace);
+  }
+}
